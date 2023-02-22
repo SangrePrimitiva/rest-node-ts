4d40d75136274d81a7946872e2af043c1c29b4c4
Juan Gagliardo <juan.sangreprimitiva@gmail.com>
2023-02-22 06:42:51 -0300

    initial

--- src/routes/post/pong/route.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response } from 'express';
-
-const routeHandler = (request: Request, response: Response) => {
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const responseJson = request.body;
-	response.send(responseJson);
-};
-export { routeHandler };
